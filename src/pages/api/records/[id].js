daa194c584e2a080b6a8481e0744792740b884ed
sanbinary <sanbinary.code@gmail.com>
2023-04-12 22:31:05 +0530

    Create a dynamic route for records

---
+++ src/pages/api/records/[id].js
@@ -0,0 +1,29 @@
+import connectToDatabase from '@/lib/mongodb'
+import Record from '@/models/record'
+
+export default async function handler(req, res) {
+  try {
+    await connectToDatabase()
+    const { id } = req.query
+    switch (req.method) {
+      case 'GET':
+        try {
+          const record = await Record.findOne({ _id: id }).exec()
+          if (!record) {
+            return res.status(404).json({ message: 'Record not found' })
+          }
+          res.status(200).json(record)
+        } catch (error) {
+          return res.status(400).json({ message: 'Invalid ID provided' })
+        }
+        break
+      default:
+        res.setHeader('Allow', ['GET'])
+        res.status(405).json({ message: `Method ${req.method} not allowed` })
+        break
+    }
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: 'Internal server error' })
+  }
+}
