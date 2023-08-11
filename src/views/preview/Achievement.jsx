120f514990123d81c2fbeacbcd7a490d6e3d1b75
sanbinary <sanbinary.code@gamil.com>
2023-08-11 22:33:18 +0530

    fix: Resolve issue with Puppeteer PDF preview

--- src/views/preview/Achievement.jsx
+++ src/views/preview/Achievement.jsx
@@ -1,28 +1,28 @@
-import { StyleSheet, View, Text } from '@react-pdf/renderer'
-import { Fragment } from 'react'
-import PointText from '@/components/PointText'
-import Section from '@/components/Section'
-import SectionTitle from '@/components/SectionTitle'
-import { useFormData } from './FormDataContext'
-import SSContainer from '@/components/SSContainer'
-import ConditionalRender from '@/components/ConditionalRender'
+// import { StyleSheet, View, Text } from '@react-pdf/renderer'
+// import { Fragment } from 'react'
+// import PointText from '@/components/PointText'
+// import Section from '@/components/Section'
+// import SectionTitle from '@/components/SectionTitle'
+// import { useFormData } from './FormDataContext'
+// import SSContainer from '@/components/SSContainer'
+// import ConditionalRender from '@/components/ConditionalRender'
 
-const styles = StyleSheet.create({})
+// const styles = StyleSheet.create({})
 
-const Achievement = () => {
-  const data = useFormData()
-  return (
-    <Section>
-      <SectionTitle title='ACHIEVEMENT' />
-      <SSContainer>
-        <ConditionalRender value={data.achievement}>
-          {data.achievement.map((value, index) => (
-            <Fragment key={index}>{value.value && <PointText>{value.value}</PointText>}</Fragment>
-          ))}
-        </ConditionalRender>
-      </SSContainer>
-    </Section>
-  )
-}
+// const Achievement = () => {
+//   const data = useFormData()
+//   return (
+//     <Section>
+//       <SectionTitle title='ACHIEVEMENT' />
+//       <SSContainer>
+//         <ConditionalRender value={data.achievement}>
+//           {data.achievement.map((value, index) => (
+//             <Fragment key={index}>{value.value && <PointText>{value.value}</PointText>}</Fragment>
+//           ))}
+//         </ConditionalRender>
+//       </SSContainer>
+//     </Section>
+//   )
+// }
 
-export default Achievement
+// export default Achievement
