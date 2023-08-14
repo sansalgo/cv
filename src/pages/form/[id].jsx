944f70cdbdcf962bcd0f7a4e139cd5e63637afc6
sanbinary <sanbinary.code@gamil.com>
2023-08-14 22:21:59 +0530

    WIP: Add functionality to change or edit filename

--- src/pages/form/[id].jsx
+++ src/pages/form/[id].jsx
@@ -1,8 +1,23 @@
 import reverseFormatRecord from '@/utils/reverse-format-record'
 import Form from '@/views/form'
 import axios from 'axios'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider, createTheme, styled } from '@mui/material/styles'
 
-export default ({ record }) => <Form record={record} />
+// const extendTheme = theme =>
+//   createTheme({
+//     ...theme,
+//     components: {
+//       ...theme.components,
+//       MuiCardHeader
+//     }
+//   })
+
+export default ({ record }) => (
+
+    <Form record={record} />
+
+)
 
 export const getServerSideProps = async ({ params, req }) => {
   const { data: record } = await axios.get(`http://localhost:3000/api/records/${params.id}`, {
