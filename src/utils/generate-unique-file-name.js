a31f1e2886a6e71001f6abcb85079c03740a9326
sanbinary <sanbinary.code@gamil.com>
2023-10-02 21:08:20 +0530

    fix: Resolve issue with file path in generateUniqueFileName

--- src/utils/generate-unique-file-name.js
+++ src/utils/generate-unique-file-name.js
@@ -1,7 +1,9 @@
 import fs from 'fs'
+import path from 'path'
 
 const generateUniqueFileName = () => {
-  const words = fs.readFileSync('src/data/wordlist10000.txt', 'utf-8').split('\n')
+  const filePath = path.join(process.cwd(), 'src/data/dictionary')
+  const words = fs.readFileSync(filePath, 'utf-8').split('\n')
   const randomWord0 = words[Math.floor(Math.random() * words.length)]
   const randomWord1 = words[Math.floor(Math.random() * words.length)]
   const randomString = Math.random().toString(36).substring(2, 8)
